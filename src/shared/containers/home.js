3ca9a08decae9da7c4535fd8a12dc98fb69d424b
Shouvik Mukherjee <contact@ishouvik.com>
2018-05-01 21:04:47 +0530

    Add HomeHero component to Home container

--- src/shared/containers/home.js
+++ src/shared/containers/home.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import DefaultLayout from '../layouts/default';
+import HomeHero from '../components/homeHero';
 
 export class Home extends Component {
   render() {
     return (
       <DefaultLayout>
-        <h1>Welcome to React</h1>
+        <HomeHero />
       </DefaultLayout>
     )
   }
