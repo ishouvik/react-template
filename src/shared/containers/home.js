cf6f9b2986139ce0c1a0e7ce5d04ca02ec4bd085
Shouvik Mukherjee <contact@ishouvik.com>
2018-05-01 20:24:56 +0530

    Add Home container

---
+++ src/shared/containers/home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import DefaultLayout from '../layouts/default';
+
+export class Home extends Component {
+  render() {
+    return (
+      <DefaultLayout>
+        <h1>Welcome to React</h1>
+      </DefaultLayout>
+    )
+  }
+}
